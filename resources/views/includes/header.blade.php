9862d39d2fa0f6cbcc9bf4478c05dea02ddf2d86
FrancescoCina <francesco.cina04@gmail.com>
2021-10-01 16:33:45 +0200

    Fatto header.blade.php collegato a main.layout con @include

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,40 @@
+<header>
+    <div class="header-top">
+        <div class="card-info">
+            Dc Power Visa 
+        </div>
+        <div class="additional-infor">Additional DC Sites</div>
+
+    </div>
+    <div class="container">
+        <div class="header-bottom">
+            <div class="logo">
+                <img src="{{ asset('images/dc-logo.png') }}" alt="Logo">
+            </div>
+            <div class="navbar">
+                <nav>
+                    <ul>
+                        <li><a href="">Characters</a></li>
+                        <li><a href="">Comics</a></li>
+                        <li><a href="">Movies</a></li>
+                        <li><a href="">Tv</a></li>
+                        <li><a href="">Games</a></li>
+                        <li><a href="">Collectibles</a></li>
+                        <li><a href="">Videos</a></li>
+                        <li><a href="">Fans</a></li>
+                        <li><a href="">News</a></li>
+                        <li><a href="">Shop</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="search">
+                <input type="search" class="search-field" placeholder="Search">
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+</header>
